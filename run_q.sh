c026bacf34da30dcd425db6beb0e74104e34199e
Kristian Kjelmann <kjelmann@live.dk>
2025-12-12 11:45:12 +0100

    promts and bash for running

---
+++ run_q.sh
@@ -0,0 +1,8 @@
+python /home/ubuntu/ragstuff/search_lit.py \
+    --input_dir "/home/ubuntu/ragstuff/output/pdf_embedded" \
+    --query_txt "/home/ubuntu/ragstuff/prompts/population/pop_rag-query.txt" \
+    --min_k 500 \
+    --filter_noisy_chunks \
+    --open_browser #\
+    #--custom_prompt "/home/ubuntu/ragstuff/prompts/population/pop_gpt-prompt.txt" \
+    #--prompt_gpt
